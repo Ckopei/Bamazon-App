d6b5e98140e141ddbe1d3c6f034198e0aaee0be5
Ckopei <cameron@kopels.com>
2019-10-06 17:14:46 -0400

    Structure set up for manager portal app.

--- bamazonManager.js
+++ bamazonManager.js
@@ -9,7 +9,6 @@ var connection = mysql.createConnection({
     password: "rootroot",
     database: "bamazon_db"
 });
-
 // connect to the mysql server and sql database
 connection.connect(function (err) {
     if (err) throw err;
@@ -17,20 +16,53 @@ connection.connect(function (err) {
     start();
 });
 
-// * List a set of menu options:
+function start() {
+    inquirer
+    .prompt({
+        name: "managerPortal",
+        type: "list",
+        message: "What department would you like?",
+        choices: ["View Products for Sale", "View Low Inventory", "Add to Inventory", "Add New Product"]
+    })
+    .then(function (answer) {
+        switch (answer.managerPortal) {
+            case "View Products for Sale":
+                viewAll();
+                break;
+            case "View Low Inventory":
+                viewLow();
+                break;
+            case "Add to Inventory":
+                addInv();
+                break;
+            case "Add New Product":
+                addNew();
+                break;
+            default:
+                break;
+        }
+    });
+}
 
-//     * View Products for Sale
-    
-//     * View Low Inventory
-    
-//     * Add to Inventory
-    
-//     * Add New Product
-
+function viewAll() {
 //   * If a manager selects `View Products for Sale`, the app should list every available item: the item IDs, names, prices, and quantities.
+}
 
+function viewLow() {
 //   * If a manager selects `View Low Inventory`, then it should list all items with an inventory count lower than five.
+}
 
+function addInv() {
 //   * If a manager selects `Add to Inventory`, your app should display a prompt that will let the manager "add more" of any item currently in the store.
+}
 
+function addNew() {
 //   * If a manager selects `Add New Product`, it should allow the manager to add a completely new product to the store.
+}
+
+
+
+
+
+
+
